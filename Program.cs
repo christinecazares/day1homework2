37c6ff8f3632ec3577363e6398c035e765cf61e2
Christine Cazares <3cccazares@gmail.com>
2015-08-19 08:59:26 -0500

    tryed did not work

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week1day1contrainan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var wordFound = "flase";
+
+            while (String.IsNullOrWhiteSpace())
+            {
+                Console.WriteLine("wordFound");
+                wordFound = Console.ReadLine();
+
+                Console.WriteLine("");
+            }
+        }
+    }
+}
